4eabcf1edc3af53c0f28583ef937327e286855c5
elizabeth koch <elk677@g.harvard.edu>
2024-09-20 22:29:15 -0400

    hw2

--- src/lecture.ts
+++ src/lecture.ts
@@ -14,4 +14,12 @@ type Animal = {
   name: string;
 };
 
-//////// You can copy and past some of the examples in here
+const greetMessage = (p: Person | Animal): string => {
+  if ("type" in p) {
+    // p is Animal, Person doesn't have a type property
+    return 'Hello ${p.name}, you are a ${p.type}';
+  } else {
+    // p is Person, Animal doesn't have type city
+    return 'Hello ${p.name}, you live in ${p.city}';
+  }
+}
